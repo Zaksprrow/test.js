c99a98686ade107c2324aeb5b5d1b9f09330bb48
Zaksprrow <raushanc371@gmail.com>
2024-08-24 04:40:21 +0000

    string

---
+++ basic/05_string.js
@@ -0,0 +1,27 @@
+//const name = "Raushan";
+//const repoCount= 50;
+
+//console.log(name +repoCount+"addvalue");
+//console.log(`hello my name is ${name} and my repo is ${repoCount}`);
+
+
+//xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+const gameName = new String("Raushanhp")
+// console.log(gameName.__proto__);
+
+//console.log(gameName[0]);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLocaleLowerCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('n'));
+
+// const newstring = gameName.substring(0,5);
+// console.log(newstring);
+
+
+// const anotherstring = gameName.slice(1,4).length;
+// console.log(anotherstring);
+
+
+
