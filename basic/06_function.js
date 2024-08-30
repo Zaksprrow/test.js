6535f387628cc190ee25f45f0be07b449ddd72dd
Zaksprrow <raushanc371@gmail.com>
2024-08-30 09:59:39 +0000

    function update

---
+++ basic/06_function.js
@@ -0,0 +1,89 @@
+// function Raushan(){
+//     console.log("hello"+this.name)
+// }
+// const person = {
+//     name: "Raushan",
+// }
+// Raushan.call(person)
+// function sum(a,b){
+//     return a+b;
+// }
+
+                                      
+// console.log(sum.apply(null,[5,10]))
+// let person1={
+//     name: "Raushan",
+//     greet: function(){
+//         console.log("hello"+this.name)
+//     }
+// };
+// let a = person1.greet.bind(person1);
+// a();
+
+// hell my name is rahul i am 25 years old
+
+
+
+// function Raushan(){
+//     console.log("hello my "+this.name+" i am 19 years old")
+// }
+// const person = {
+//     name: "Raushan",
+// }
+// Raushan.call(person)
+
+
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// function kanishk(){
+//     console.log("hello"+this.name)
+// }
+// const person = {
+//     name: "kanishk",
+// }
+// kanishk.call(person)
+// function sum(a,b){
+//     return a+b;
+// }
+// console.log(sum.apply(null,[5,10]))
+
+// let person1={
+//     name: "Raushan",
+//     greet: function(){
+//         console.log("hello"+this.name)
+//     }
+// };
+// let a = person1.greet.apply(person1);
+// a();
+
+
+// const person = {
+//     fullName: function() {
+//       return this.firstName + " " + this.lastName;
+//     }
+//   }
+  
+//   const person1 = {
+//     firstName:"John",
+//     lastName: "Doe"
+//   }
+  
+//  person.fullName.apply(person1); 
+
+
+const persion = {
+    intro: function(){
+        return this.first+""+this.last;
+        
+    }
+}
+const persion1={
+    first: "Raushan",
+    last: "Kumar"
+}
+
+console.log(persion.intro.apply(persion1));
