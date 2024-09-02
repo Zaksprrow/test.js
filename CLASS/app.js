04e86cee2989fb9d6de1668694a6051fdf995313
Zaksprrow <raushanc371@gmail.com>
2024-09-02 09:57:45 +0000

    new up

---
+++ CLASS/app.js
@@ -0,0 +1,15 @@
+// let btn = document.querySelector("button")
+// btn.addEventListener("click" , function(){
+//    btn.style.backgroundColor="#00000" color="yello";
+// })
+
+//document.getElementById("demo").innerHTML="Hello world this is Raushan f//document.querySelector("p").innerHTML="Hello world this is Raushan from BR"
+
+// const p =document.getElementsByTagName("p") 
+// document.getElementById("demo").innerHTML="Raushan"+p[0].innerHTML;
+
+
+// let x=document.getElementsByClassName("intero");
+// document.getElementById("demo").innerText="hi this is "+x[1].innerHTML;
+
+
